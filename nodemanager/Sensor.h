a07fdf3bc4c08cbc0574327d10f7641a98d113f6
Andrew Andrianov <andrew@ncrmnt.org>
2021-01-09 17:08:40 +0300

    Sensor: Implement getChild(id,type)
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- nodemanager/Sensor.h
+++ nodemanager/Sensor.h
@@ -64,6 +64,7 @@ public:
 	List<Child*> children;
 	// return the child object based on the provided child_id
 	Child* getChild(uint8_t child_id);
+	Child* getChild(uint8_t child_id, uint8_t ctype);
 	// register a child
 	void registerChild(Child* child);
 #if NODEMANAGER_INTERRUPTS == ON
